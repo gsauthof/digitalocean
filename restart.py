84fb61ad892603a2659c3e6a70eaae7687b4e05e
Georg Sauthoff <mail@georg.so>
2016-06-25 11:21:42 +0200

    Use exponential backoff everywhere
    See also: https://en.wikipedia.org/wiki/Exponential_backoff
    
    I know that there is also the `retrying` package, but using an iterator
    makes more sense in this context.
    
    (`retrying` implements a decorator)

--- restart.py
+++ restart.py
@@ -20,7 +20,44 @@ import subprocess
 import time
 import pprint
 
+import random
+import unittest
+
 bearer = os.getenv('bearer')
+
+
+class exp_backoff:
+    def __init__(self, n=5, duration = 0, ceiling=10):
+        self.n = n
+        self.duration = duration
+        self.ceiling = ceiling
+        self.i = 0
+        self.v = 2
+        self.wait_time = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.i < self.n or self.wait_time < self.duration:
+            r = random.uniform(0, self.v-1)
+            self.wait_time = self.wait_time + r
+            if self.i + 1 < self.ceiling:
+                self.v = self.v * 2
+            self.i = self.i + 1
+            return r
+        else:
+            raise StopIteration()
+
+class Test_Exp_Backoff(unittest.TestCase):
+    def test_count(self):
+        self.assertEqual(list(exp_backoff(10)).__len__(), 10)
+        self.assertEqual(list(exp_backoff(3)).__len__(), 3)
+    def test_below_ceiling(self):
+        self.assertTrue([ x for x in list(exp_backoff(10000)) if x > 1020 ])
+    def test_above_ceiling(self):
+        self.assertFalse([ x for x in list(exp_backoff(10000)) if x > 1023 ])
+    def test_duration(self):
+        self.assertTrue(sum(exp_backoff(1, duration=30)) > 30)
+
 
 def mk_headers(bearer):
   return { 'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(bearer) }
@@ -57,7 +94,7 @@ def change_kernel(bearer, droplet_id, kernel_id):
 
 def verify_action_completed_prime(bearer, action_id):
   print('Verifying action completed, id: {}'.format(action_id))
-  for i in range(10):
+  for i in exp_backoff(duration=30):
     r = requests.get('https://api.digitalocean.com/v2/actions/{}'.format(action_id),
         headers = mk_headers(bearer))
     if r.status_code == 200:
@@ -66,7 +103,7 @@ def verify_action_completed_prime(bearer, action_id):
         return
     elif r.status_code != 201:
       raise Exception(r.text)
-    time.sleep(3)
+    time.sleep(i)
   raise Exception('Action {} still not completed'.format(action_id))
 
 def verify_action_completed(bearer, action, desc):
@@ -87,7 +124,7 @@ def shutdown(hostname):
 
 def verify_status(bearer, droplet_id, value):
   print('Verifying status {} on droplet id: {}'.format(value, droplet_id))
-  for i in range(10):
+  for i in exp_backoff(duration=30):
     r = requests.get('https://api.digitalocean.com/v2/droplets/{}'.format(droplet_id),
         headers = mk_headers(bearer))
     if r.status_code == 200:
@@ -96,7 +133,7 @@ def verify_status(bearer, droplet_id, value):
         return
     else:
       raise Exception(r.text)
-    time.sleep(3)
+    time.sleep(i)
   raise Exception('Droplet {} still not {}'.format(droplet_id, value))
 
 def power_on(bearer, droplet_id):
@@ -111,8 +148,7 @@ def power_on(bearer, droplet_id):
 
 def verify_running_kernel(expected_version, hostname):
   print('Verifying {} is running: {}'.format(hostname, expected_version))
-  sleep_time = 0.5
-  for i in range(3):
+  for i in exp_backoff(3):
     try:
       actual_version = subprocess.check_output(['ssh', hostname, 'uname', '-r']).decode('ascii').strip()
       if expected_version != actual_version:
@@ -121,9 +157,7 @@ def verify_running_kernel(expected_version, hostname):
       return
     except subprocess.CalledProcessError as e:
       if e.returncode == 255:
-        time.sleep(sleep_time)
-        sleep_time = sleep_time * 2
-        continue
+        time.sleep(i)
       else:
         raise
   raise Exception('ssh command failed several times')
